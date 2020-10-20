c98ae9409a587a47d85396a4636e9e71cf74a04f
Tuan Nguyen <tuannguyen@nine.com.au>
2020-10-20 23:12:16 +1100

    jest-aws-mock fix

--- failure/method2.test.js
+++ failure/method2.test.js
@@ -1,23 +1,16 @@
-import { publishMessage } from './sns-service';
-import SNS from 'aws-sdk/clients/sns';
-
-const AWSMock = require('jest-aws-sdk-mock');
+import * as AWSMock from 'jest-aws-sdk-mock';
 AWSMock.mock('SNS', 'publish', 'result-message');
 
-describe('With jest-aws-sdk-mock', () => {
-  beforeEach(() => {
-    process.env = Object.assign(process.env, { 
-      AWS_REGION: 'eu-west-1',
-      SNS_TOPIC_ARN: 'arn:sns'
-    });
-  });
+const { publishMessage } = require('./sns-service');
+const SNS = require('aws-sdk/clients/sns');
 
+describe('With jest-aws-sdk-mock', () => {
   it('should publish message', async  () => {
     const sns = new SNS();
     const type = 'MESSAGE_TEST';
     const data = { id: '123' };
 
-    const resut = await publishMessage(type, data);
+    const result = await publishMessage(type, data);
 
     expect(result).toBe('result-message');
     expect(sns.publish).toHaveBeenCalledTimes(1);
